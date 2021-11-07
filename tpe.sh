de376cc61f0d96b8d738cad1f3328f68542b5d8a
ImNotGone <gmartone@itba.edu.ar>
2021-11-07 13:12:55 -0300

    Update tpe.sh

--- tpe.sh
+++ tpe.sh
@@ -14,66 +14,64 @@ is_decimal_positive() {
   return $?
 }
 
-white="\e[m"
-red="\e[31m"
-green="\e[32m"
-orange="\e[38;5;208m"
+declare -r ALL_VALUES=0
+declare -r WHITE="\e[m"
+declare -r RED="\e[31m"
+declare -r GREEN="\e[32m"
+declare -r ORANGE="\e[38;5;208m"
 
 errno=0
-declare -r ALL_VALUES=0
-# checkear si el valor es > 0
+qty=$ALL_VALUES
 case $# in
-0) qty=$ALL_VALUES ;; 
+0);; 
 1) 
-    if is_decimal_num $1
+  if is_decimal_num $1
+  then
+    if is_decimal_positive $1
     then
-      if is_decimal_positive $1
-      then
-        qty=$1
-      else
-        errno=1
-      fi
+      qty=$1
     else
-        errno=2
+      errno=1
     fi
-    ;;
-*) 
-    errno=3
-;;
+  else
+    errno=2
+  fi
+  ;;
+*) errno=3 ;;
 esac
 
 if [ $errno -eq 0 ]
 then
-  echo -e "${green}[INFO ]${white} Downloading airports.xml"
+  echo -e "${GREEN}[INFO ]${WHITE} Downloading airports.xml"
   # `curl https://airlabs.co/api/v9/airports.xml?api_key=${AIRLABS_API_KEY} > airports.xml`
-  echo -e "${green}[INFO ]${white} Downloading countries.xml"
+  echo -e "${GREEN}[INFO ]${WHITE} Downloading countries.xml"
   # `curl https://airlabs.co/api/v9/countries.xml?api_key=${AIRLABS_API_KEY} > countries.xml`
-  echo -e "${green}[INFO ]${white} Downloading flights.xml"
+  echo -e "${GREEN}[INFO ]${WHITE} Downloading flights.xml"
   # `curl https://airlabs.co/api/v9/flights.xml?api_key=${AIRLABS_API_KEY} > flights.xml`
 else
-  echo -e "${red}[ERROR]${white} Api data won't be downloaded, no data will be processed"
+  echo -e "${RED}[ERROR]${WHITE} Api data won't be downloaded, errors will be reported"
 fi
 
-echo -e "${green}[INFO ]${white} Processing *.xml"
+echo -e "${GREEN}[INFO ]${WHITE} Processing *.xml"
 `java net.sf.saxon.Query ./extract_data.xq errno=${errno} > ./flights_data.xml`
-echo -e "${green}[INFO ]${white} File flights_data.xml created"
+echo -e "${GREEN}[INFO ]${WHITE} File flights_data.xml created"
 
-echo -e "${green}[INFO ]${white} Processing flights_data.xml"
+echo -e "${GREEN}[INFO ]${WHITE} Processing flights_data.xml"
 `java net.sf.saxon.Transform -s:flights_data.xml -xsl:generate_report.xsl -o:report.tex qty=${qty} ALL_VALUES=${ALL_VALUES}`
-echo -e "${green}[INFO ]${white} File report.tex created"
+echo -e "${GREEN}[INFO ]${WHITE} File report.tex created"
 
 if [ $errno -ne 0 ]
 then
-  echo -e "\n${red}[ERROR]${white} Data was not processed correcty"
-  echo -e "\n${orange}[ERRNO] Error number = ${errno}${white}"
+  echo -e "\n${RED}[ERROR]${WHITE} Data was not processed correcty, errors were reported"
+  echo -e "\n${ORANGE}[ERRNO] Error number = ${errno}${WHITE}"
   
   case $errno in
-  1) echo -e "${orange}[DESC ] Decimal number must be greater than 0${white}";;
-  2) echo -e "${orange}[DESC ] The argument recived was not a decimal number${white}";;
-  3) echo -e "${orange}[DESC ] Maximum argument count exceeded${white}";;
-  *) echo -e "${orange}[DESC ] Unknown error${white}";;
+  1) echo -e "${ORANGE}[DESC ] Decimal number must be greater than 0${WHITE}";;
+  2) echo -e "${ORANGE}[DESC ] The argument recived was not a decimal number${WHITE}";;
+  3) echo -e "${ORANGE}[DESC ] Maximum argument count exceeded${WHITE}";;
+  *) echo -e "${ORANGE}[DESC ] Unknown error${WHITE}";;
   esac
 else
-  echo -e "\n${green}[INFO ]${white} Data processing finished no errors were found"
+  echo -e "\n${GREEN}[INFO ]${WHITE} Data processing finished no errors were found"
 fi
 exit $errno
